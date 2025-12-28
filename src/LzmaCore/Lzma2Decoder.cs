25a29eb9c109782773fa3f3c49328059f46a25f0
Артемий Соболев <titeha@yandex.ru>
2025-12-29 00:03:59 +0700

    Ообновление концепции, начали заново

--- src/LzmaCore/Lzma2Decoder.cs
+++
@@ -1,6 +0,0 @@
-namespace LzmaCore;
-
-public sealed class Lzma2Decoder
-{
-  public void Decode(Stream input, Stream output, Lzma2Options options, CancellationToken ct = default) => throw new NotImplementedException();
-}
