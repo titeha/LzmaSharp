38a2ee68d91570b2910a83ff5ae06a6eb0dfa8c4
Artemy Sobolev <titeha@yandex.ru>
2026-04-19 01:13:22 +0700

    Тесты: добавить проверку числа FolderUnpackSizes в SevenZipFolderDecoder

--- tests/Lzma.Core.Tests/SevenZip/SevenZipFolderDecoderUnpackSizesValidation.Tests.cs
+++ tests/Lzma.Core.Tests/SevenZip/SevenZipFolderDecoderUnpackSizesValidation.Tests.cs
@@ -55,6 +55,76 @@ public sealed class SevenZipFolderDecoderUnpackSizesValidationTests
     Assert.Empty(output);
   }
 
+  [Theory]
+  [InlineData(1)]
+  [InlineData(3)]
+  public void DecodeFolderToArray_КоличествоFolderUnpackSizesНеСовпадаетСЧисломCoder_ВозвращаетNotSupported(
+    int unpackSizesCount)
+  {
+    SevenZipStreamsInfo streamsInfo = CreateTwoCoderStreamsInfo(
+        folderUnpackSizes: CreateUnpackSizes(unpackSizesCount));
+
+    SevenZipFolderDecodeResult result = SevenZipFolderDecoder.DecodeFolderToArray(
+        streamsInfo: streamsInfo,
+        packedStreams: [0x10, 0x20, 0x30],
+        folderIndex: 0,
+        output: out byte[] output);
+
+    Assert.Equal(SevenZipFolderDecodeResult.NotSupported, result);
+    Assert.Empty(output);
+  }
+
+  private static ulong[] CreateUnpackSizes(int count)
+  {
+    ulong[] sizes = new ulong[count];
+
+    for (int i = 0; i < sizes.Length; i++)
+      sizes[i] = 3UL;
+
+    return sizes;
+  }
+
+  private static SevenZipStreamsInfo CreateTwoCoderStreamsInfo(ulong[] folderUnpackSizes)
+  {
+    var folder = new SevenZipFolder(
+        Coders:
+        [
+          new SevenZipCoderInfo(
+            methodId: [0x00],
+            properties: [],
+            numInStreams: 1,
+            numOutStreams: 1),
+        new SevenZipCoderInfo(
+            methodId: [0x00],
+            properties: [],
+            numInStreams: 1,
+            numOutStreams: 1),
+        ],
+        BindPairs:
+        [
+          new SevenZipBindPair(InIndex: 1, OutIndex: 0),
+        ],
+        PackedStreamIndices: [0UL],
+        NumInStreams: 2,
+        NumOutStreams: 2);
+
+    var packInfo = new SevenZipPackInfo(
+        packPos: 0,
+        packSizes: [3UL]);
+
+    var unpackInfo = new SevenZipUnpackInfo(
+        folders: [folder],
+        folderUnpackSizes:
+        [
+          folderUnpackSizes,
+        ]);
+
+    return new SevenZipStreamsInfo(
+        packInfo: packInfo,
+        unpackInfo: unpackInfo,
+        subStreamsInfo: null);
+  }
+
   private static SevenZipStreamsInfo CreateStreamsInfo(ulong[][] folderUnpackSizes)
   {
     var folder = new SevenZipFolder(
